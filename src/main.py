33768e5b6415a676dfb62dad7fc0cb1ca8c7d991
MenInBlack2009 <max.braunshweiger@neja.org>
2025-11-13 17:23:02 +0000

    play with the movement

--- src/main.py
+++ src/main.py
@@ -1,30 +1,49 @@
 import pyxel
 
-W, H = 160, 120
-x, y = 10, 10
-dx, dy = 1, 1
 
+SPEED = 0.2
+GRAVITY = 0.3
+BOUNCE = 0.7
+W, H = 160, 120
+
+# initial position and velocity
+x, y = W // 2, H // 2
+vx, vy = 0, 0
 
 def update():
-    global x, y, dx, dy
-    # Quit with Q
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+   global x, y, vx, vy
 
-    # Simple movement demo
-    x += dx
-    y += dy
-    if x <= 0 or x >= W - 10:
-        dx = -dx
-    if y <= 0 or y >= H - 10:
-        dy = -dy
- 
+   # Apply gravity
+   vy += GRAVITY
+   y += vy
+  
+   if x < 100 or x > 110:
+      vx += SPEED
+      x += vx
+   else:
+       vx -= SPEED
+       x += vx
+
+   # Bounce when hitting the ground
+   ground = H - 5
+   if y > ground:
+       y = ground
+       vy = -vy * BOUNCE  # reverse and lose some energy
+
+   walls = W - 5
+   if x > walls:
+         x = walls
+         vx = -vx * BOUNCE
+
+   if x < 3:
+      x = 3
+      vx = -vx * BOUNCE
 
 def draw():
-    pyxel.cls(0)              # clear screen to color 0 (black)
-    pyxel.text(4, 4, "Hello Pyxel Web!", 7)
-    pyxel.rect(x, y, 10, 10, 11)  # small bouncing square
+   pyxel.cls(0)
+   pyxel.circ(x, y, 9, 1)
 
 
-pyxel.init(W, H, title="Pyxel Web (Codespaces)")
-pyxel.run(update, draw)
+
+pyxel.init(W,H, title="fun")
+pyxel.run(update,draw)
